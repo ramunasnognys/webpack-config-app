44ef9d14a8ee6af536287be098e9d34b382b386c
Ramunas <dev.nognys@gmail.com>
2021-03-24 21:25:14 +0100

    Minify CSS
    - Extracted CSS into own file in production

--- webpack.common.js
+++ webpack.common.js
@@ -17,15 +17,7 @@ module.exports = {
 
   module: {
     rules: [
-      {
-        test: /\.(s[ac]|c)ss$/i,
-        use: [
-          "style-loader", //3. Inject styles into DOM
-          "css-loader", //2. Turns css into commonjs
-          "sass-loader", //1. Turns sass into css
-        ],
-      },
-
+      
       // Images
       {
         test: /\.(?:ico|svg|gif|png|jpg|jpeg)$/i,
--- webpack.prod.js
+++ webpack.prod.js
@@ -2,14 +2,30 @@ const path = require("path");
 const common = require("./webpack.common");
 const { merge } = require("webpack-merge");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = merge(common, {
   mode: "production",
   output: {
     filename: "[name].[contenthash].bundle.js",
     path: path.resolve(__dirname, "dist"), // output directory
-  },  
+  },
 
-  plugins: [new CleanWebpackPlugin()]
+  plugins: [
+    new MiniCssExtractPlugin({ filename: "[name].[contenthash].css" }),
+    new CleanWebpackPlugin(),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.(s[ac]|c)ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader, //3. Extract CSS into files
+          "css-loader", //2. Turns css into commonjs
+          "sass-loader", //1. Turns sass into css
+        ],
+      },
+    ]
+  }
 });
