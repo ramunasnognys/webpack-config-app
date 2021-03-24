218e86e879dfb0faa34478c57b61e62d007df6f0
Ramunas <dev.nognys@gmail.com>
2021-03-24 08:21:42 +0200

    Cache busting and HtmlWebpackPlugin
    - Configured webpack to use in bundle file name
    - This caused a problem without script tag in index.html
    - We installed HtmlWebpackPlugin to help us generate a newindex.html file
    - It automatically include the correct script tag  at the bottom
    - We created the template file that passed in called template.html
    - We deleted the original index.html because we don't need it anymore
    - Make sure you are opening dist/index.html now to view the app

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,20 @@
 const path = require("path");
 
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
   mode: "development",
   entry: "./src/index.js", // bundle's entry point
   output: {
+    filename: "[name].[contenthash].js",
     path: path.resolve(__dirname, "dist"), // output directory
-    filename: "[name].js", // name of the generated bundle
   },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      // template: "index.html",
+      template: "./src/template.html",
+      inject: "body",
+    }),
+  ],
 };
