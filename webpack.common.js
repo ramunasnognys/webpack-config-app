3fa0a7d220c7654de77ff6977b1e6b8f6f920d80
Ramunas <dev.nognys@gmail.com>
2021-03-25 06:18:28 +0100

    added vendor.js file

--- webpack.common.js
+++ webpack.common.js
@@ -2,7 +2,10 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js", // bundle's entry point
+  entry: {
+    main: "./src/index.js", // bundle's entry point
+    vendor: "./src/vendor.js", // bootstrap, reset.css etc..
+  },
 
   plugins: [
     new HtmlWebpackPlugin({
--- webpack.prod.js
+++ webpack.prod.js
@@ -5,7 +5,7 @@ const { merge } = require("webpack-merge");
 module.exports = merge(common, {
   mode: "production",
   output: {
-    filename: "[name].[contenthash].js",
+    filename: "[name].[contenthash].bundle.js",
     path: path.resolve(__dirname, "dist"), // output directory
   },  
 });
